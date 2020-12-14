886c57b2668c7dc572aef29cba07fb49b746477d
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-12-14 09:49:30 -0600

    Case sens test

--- store/Architecture.js
+++ store/Architecture.js
@@ -1,11 +1,11 @@
 import links from './Links';
 import img from '../pics/OCM-transparent1.png';
 import img1 from '../pics/Environmental Architecture/IMG_1490.jpeg';
-import img3 from '../pics/Environmental Architecture/IMG_6653.jpg';
+import img3 from '../pics/Environmental Architecture/IMG_6653.JPG';
 import img5 from '../pics/Environmental Architecture/IMG_6681.jpeg';
 import img6 from '../pics/Environmental Architecture/IMG_6679.jpeg';
 import img7 from '../pics/Environmental Architecture/IMG_6673.jpeg';
-import img8 from '../pics/Environmental Architecture/IMG_6670.jpg';
+import img8 from '../pics/Environmental Architecture/IMG_6670.JPG';
 import img9 from '../pics/Environmental Architecture/IMG_6684.jpeg';
 
 
